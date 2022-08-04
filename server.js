1a62dd8f54e00ab6aea09793dcaa951e5afabed4
yenmangu <robert.shelford@googlemail.com>
2022-08-04 12:09:30 +0100

    people - in - space

---
+++ server.js
@@ -0,0 +1,18 @@
+// server.js
+
+const http = require('http');
+
+http
+    .get('http://api.open-notify.org/astros.json', resp => {
+        let data = ''
+
+        resp.on('data', chunk => {
+            data += chunk
+        })
+
+        resp.on('end', () => {
+            let PeopleData = JSON.parse(data)
+            console.log(PeopleData)
+        })
+
+    })
