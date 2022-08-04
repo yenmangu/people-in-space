3fe86eef1490fc3e608e700895810b56a6193911
yenmangu <robert.shelford@googlemail.com>
2022-08-04 15:04:37 +0100

    people in space server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // server.js
 
 const http = require('http');
+const hostname = '127.0.0.1'
+const port = 8000
 
 http
     .get('http://api.open-notify.org/astros.json', resp => {
@@ -11,8 +13,24 @@ http
         })
 
         resp.on('end', () => {
-            let PeopleData = JSON.parse(data)
-            console.log(PeopleData)
+            let peopleData = JSON.parse(data)
+            console.log(peopleData)
+
+            const server = http.createServer((req, res) => {
+                res.statusCode = 200
+
+                res.setHeader('Content-Type', 'application/json')
+                res.setHeader('Access-Control-Allow-Origin', '*')
+
+                res.end(JSON.stringify(peopleData.people))
+            })
+
+            server.listen(port, hostname, () => {
+                console.log(`Server running at http://${hostname}:${port}/`)
+            })
         })
 
+        .on('error', err => {
+            console.log("Error: ", err.message)
+        })
     })
